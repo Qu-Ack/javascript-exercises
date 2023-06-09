400b157d46c2623e13b6579afd8902b50af1fb02
Daksh Sangal <134773611+Qu-Ack@users.noreply.github.com>
2023-06-09 02:10:21 -0400

    All excercises done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  let result = ((tempF -32) * 5)/9;
+  let roundedResult = Math.round(result * 10)/10;
+  return roundedResult;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  let fResult = ((tempC * 9)/5) + 32;
+  let roundedFResult = Math.round(fResult * 10)/10;
+  return roundedFResult
 };
 
 // Do not edit below this line
