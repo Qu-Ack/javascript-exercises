5f8ea4b891e175ec1a76b07c6f6694bb5c715a1e
Daksh Sangal <134773611+Qu-Ack@users.noreply.github.com>
2023-06-09 01:36:45 -0400

    Completed sumAll excercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  let result = 0;
+  if (num1 < 0 || num2 < 0) {
+    return "ERROR";
+  }
 
+  if (Number.isInteger(num1) && Number.isInteger(num2)) {
+    if (num2 > num1) {
+      for (let i = num1; i <= num2; i++) {
+        result += i;
+      }
+    } else if (num1 > num2) {
+      for (let i = num2; i <= num1; i++) {
+        result += i;
+      }
+    }
+  }
+  else {
+    return "ERROR";
+  }
+
+  return result;
 };
 
 // Do not edit below this line
