0bfed8c6e91d76057f9658698ee32b849328cea8
Daksh Sangal <134773611+Qu-Ack@users.noreply.github.com>
2023-06-09 01:13:32 -0400

    EDIT js files completed till remove from array

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+     let splitString = word.split("")
+     let reverseString = splitString.reverse();
+     let result = reverseString.join("");
+     return result;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, ...args) {
+  for (let i = 0; i < args.length; i++) {
+    let index = arr.indexOf(args[i]);
+    if (index > -1) {
+      arr.splice(index, 1);
+    }
+  }
+  return arr;
 };
 
 // Do not edit below this line
